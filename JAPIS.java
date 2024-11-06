12a8ba2217d57960773a47a30b8aab2f16a84565
Junho Choi <127073705+JujakPRO@users.noreply.github.com>
2024-11-06 19:27:36 +0900

    Add files via upload

--- JAPIS.java
+++ JAPIS.java
@@ -1,3 +1,5 @@
+// GitHub Public Repository (full-source-code): https://github.com/JujakPRO/INU-Programming-MidtermExam
+
 // Project #1, Just A good Planet Information System
 
 import java.util.Scanner;
@@ -11,6 +13,7 @@ public class midtermClass {
         String planetData[][] = new String[10][5]; // 이름, 반지름, 자전 주기, 공전 주기, 태양으로부터의 거리 데이터를 저장하는 2차원 배열
         int planetCount[] = new int[1]; // 입력된 행성의 개수를 저장하는 1차원 배열
 
+        inputPlanetData(scanner, planetData, planetCount); // main 하위 메서드인 public static void inputPlanetData(...)을 모듈화 함.
 
         String planetName[] = new String[planetCount[0]]; // 2차원 배열 planetData[][]을 별도의 배열로 저장 (String)
 
@@ -21,7 +24,7 @@ public class midtermClass {
         double planetRadius[] = new double[planetCount[0]]; // 2차원 배열 planetData[][]을 별도의 배열로 저장 (double)
 
         for (int i = 0; i < planetCount[0]; i++) {
-            planetRadius[i] = Double.parseDouble(planetData[i][1]);
+            planetRadius[i] = Double.parseDouble(planetData[i][1]); // planetData[][]는 String(문자열)이므로, Double 형으로 변환이 필요하기 때문에 Double.parseDouble로 변환.
         }
         
         double rotationPeriod[] = new double[planetCount[0]]; // 2차원 배열 planetData[][]을 별도의 배열로 저장 (double)
@@ -41,8 +44,6 @@ public class midtermClass {
         for (int i = 0; i < planetCount[0]; i++) {
             distancefromSun[i] = Double.parseDouble(planetData[i][4]);
         }
-        
-        inputPlanetData(scanner, planetData, planetCount); // main 하위 메서드인 public static void inputPlanetData(...)을 모듈화 함.
 
         System.out.println("JAPIS에게 묻고 싶은 정보는?"
                 + "\n1: 태양과 가장 가까운 행성"
@@ -52,25 +53,25 @@ public class midtermClass {
                 + "\n5: 자전 주기가 가장 짧은 행성");
 
         int option = scanner.nextInt(); // 옵션 1부터 옵션 5까지 입력 받음
-        switch (option) {
+        switch (option) { // switch-case문 (if~else문 대체)
             case 1:
-                closestPlanet(planetData, planetCount[0]);
+                closestPlanet(distancefromSun, planetName, planetCount[0]);
                 break;
                 
             case 2:
-            	fartestPlanet(planetData, planetCount[0]);
+            	fartestPlanet(distancefromSun, planetName, planetCount[0]);
             	break;
             	
             case 3:
-            	avgDistance(planetData, planetCount[0]);
+            	avgDistance(distancefromSun, planetCount[0]);
             	break;
             	
             case 4:
-            	largestRadius(planetData, planetCount[0]);
+            	largestRadius(planetRadius, planetName, planetCount[0]);
             	break;
             	
             case 5:
-            	shortestRtPeriod(planetData, planetCount[0]);
+            	shortestRtPeriod(rotationPeriod, planetName, planetCount[0]);
             	break;
             	
             default:
@@ -81,9 +82,6 @@ public class midtermClass {
         scanner.close();
     }
 
-    // end of line 
-    
-    
     // 행성의 데이터를 일렬로 받아 2차원 배열에 저장하는 inputPlanetData 메서드
     
     public static void inputPlanetData(Scanner scanner, String planetData[][], int planetCount[]) {
@@ -94,18 +92,18 @@ public class midtermClass {
                         + "입력을 종료하려면 'end'를 입력하세요:");
 
         while (i < 10) {
-            String input = scanner.next();
-            if (input.equals("end")) { // input에 end가 포함될 시 입력 중단
+            planetData[i][0] = scanner.next(); // 행성 이름
+
+            if (planetData[i][0].equals("end")) { // 행성 이름에 end가 포함될 시 입력 중단 (문자열)
                 break;
             }
 
-            planetData[i][0] = input; // 행성 이름
             planetData[i][1] = scanner.next(); // 반지름
             planetData[i][2] = scanner.next(); // 자전 주기
             planetData[i][3] = scanner.next(); // 공전 주기
             planetData[i][4] = scanner.next(); // 태양으로부터의 거리
 
-            scanner.nextLine();
+            scanner.nextLine(); // 개행 처리 (개행 처리로 버퍼 정리)
             
             i++; // 인덱스를 증가시킴 (행성의 개수 증가)
         }
@@ -117,39 +115,37 @@ public class midtermClass {
 
     // 태양과 가장 가까운 행성의 이름을 출력하는 메서드
     
-    public static void closestPlanet(String planetData[][], int planetCount) {
+    public static void closestPlanet(double distancefromSun[], String planetName[], int planetCount) {
         int closestDistance = 0;
 
         for (int i = 0; i < planetCount; i++) {
-            if (planetData[closestDistance][4].length() < planetData[i][4].length())
+            if (distancefromSun[closestDistance] < distancefromSun[i])
                 continue;
             else
-                closestDistance = i; // 만약 nearestDistance보다 작을 시, i를 nearestDistance에 대입
+                closestDistance = i; // 만약 closestDistance보다 작을 시, i를 closestDistance에 대입
         }
 
-        String print = planetData[closestDistance][0];
+        String print = planetName[closestDistance];
 
         System.out.print(print);
-        
     }
-
 
 	// end of line
 
 
 	// 태양과 가장 먼 행성의 이름을 출력하는 메서드
 
-	public static void fartestPlanet(String planetData[][], int planetCount) {
+	public static void fartestPlanet(double distancefromSun[], String planetName[], int planetCount) {
 		int fartestDistance = 0;
 		
 		for (int i = 0; i < planetCount; i++) {
-			if (planetData[fartestDistance][4].length() > planetData[i][4].length())
+			if (distancefromSun[fartestDistance] > distancefromSun[i])
 				continue;
 			else
 				fartestDistance = i; // 만약 fartestDistance보다 클 시, i를 fartestDistance에 대입		
 		}
 		
-		String print = planetData[fartestDistance][0];
+		String print = planetName[fartestDistance];
 		
 		System.out.print(print);
 	}
@@ -159,11 +155,11 @@ public class midtermClass {
 	
 	// 행성들의 태양과의 평균 거리를 출력하는 메서드
 	
-	public static void avgDistance(String planetData[][], int planetCount) {
+	public static void avgDistance(double distancefromSun[], int planetCount) {
 		double sum = 0;
 		
 		for (int i = 0; i < planetCount; i++) {
-			sum += Double.parseDouble(planetData[i][4]);
+			sum += distancefromSun[i];
 		}
 		
 		double avg = sum / planetCount;
@@ -176,17 +172,17 @@ public class midtermClass {
 	
 	// 반지름이 가장 큰 행성의 이름을 출력하는 메서드
 	
-	public static void largestRadius(String planetData[][], int planetCount) {
+	public static void largestRadius(double planetRadius[], String planetName[], int planetCount) {
 		int largestRadius = 0;
 		
 		for (int i = 0; i < planetCount; i++) {
-			if (planetData[largestRadius][1].length() > planetData[i][1].length())
+			if (planetRadius[largestRadius] > planetRadius[i])
 				continue;
 			else
 				largestRadius = i; // 만약 largestRadius보다 클 시, i를 largestRadius에 대입
 		}
 		
-		String print = planetData[largestRadius][0];
+		String print = planetName[largestRadius];
 		
 		System.out.print(print);
 	}
@@ -196,17 +192,17 @@ public class midtermClass {
 	
 	// 자전 주기가 가장 짧은 행성의 이름을 출력하는 메서드
 	
-	public static void shortestRtPeriod(String planetData[][], int planetCount) {
+	public static void shortestRtPeriod(double rotationPeriod[], String planetName[], int planetCount) {
 		int shortestRtPeriod = 0;
 		
 		for (int i = 0; i < planetCount; i++) {
-			if (planetData[shortestRtPeriod][2].length() < planetData[i][2].length())
+			if (rotationPeriod[shortestRtPeriod] < rotationPeriod[i])
 				continue;
 			else
-				shortestRtPeriod = i; // 만약 shortestRtPeriod보다 작을 시, i를 shortestPeriod에 대입
+				shortestRtPeriod = i; // 만약 shortestRtPeriod보다 작을 시, i를 shortestRtPeriod에 대입
 		}
-		
-		String print = planetData[shortestRtPeriod][0];
+
+        String print = planetName[shortestRtPeriod];
 		
 		System.out.print(print);
 	}
